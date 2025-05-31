76dfd21bcc18691bcf51e3162611d07286f4249f
Maximus <62805287+PapyRGB@users.noreply.github.com>
2025-05-31 21:38:24 +0100

    supress useless warning

--- src/retriever.py
+++ src/retriever.py
@@ -2,6 +2,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import torch
+#torch.classes.__path__ = [] # add this line to manually set it to empty.
+torch.classes.__path__ = [os.path.join(torch.__path__[0], torch.classes.__file__)] 
+
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from src.embed_index import load_index, load_chunks
