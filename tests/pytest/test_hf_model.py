88df4901b8b1f2e5da92bb8ee092292d8fc851b1
Maximus <62805287+PapyRGB@users.noreply.github.com>
2025-06-02 21:49:55 +0100

    needed to change absolute path due to re-ordering

--- tests/pytest/test_hf_model.py
+++ tests/pytest/test_hf_model.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from src.hf_model import query_huggingface_chat
 import pytest
